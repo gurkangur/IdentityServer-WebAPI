2a827d8a4a6906b75bc20f055945fe70e7f87023
gurkangur <gurkangur154@gmail.com>
2020-12-26 12:24:55 +0300

    Add entity type configuration with fluent api

---
+++ IdentityServer-WebAPI/Identity/Data/Configurations/ApplicationRoleClaimConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer_WebAPI.Identity.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityServer_WebAPI.Identity.Data.Configurations
+{
+    public class ApplicationRoleClaimConfiguration : IEntityTypeConfiguration<ApplicationRoleClaim>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationRoleClaim> builder)
+        {
+            builder.Ignore(p => p.ClaimType);
+            builder.Ignore(p => p.ClaimValue);
+
+
+            builder.HasKey(bc => new { bc.RoleId, bc.OperationClaimId });
+
+            builder.HasOne(bc => bc.ApplicationRole)
+                .WithMany(b => b.ApplicationRoleClaims)
+                .HasForeignKey(bc => bc.RoleId);
+
+
+            builder.HasOne(bc => bc.OperationClaim)
+               .WithMany(c => c.ApplicationRoleClaims)
+               .HasForeignKey(bc => bc.OperationClaimId);
+
+
+        }
+    }
+}
---
+++ IdentityServer-WebAPI/Identity/Data/Configurations/ApplicationUserClaimConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer_WebAPI.Identity.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityServer_WebAPI.Identity.Data.Configurations
+{
+    public class ApplicationUserClaimConfiguration : IEntityTypeConfiguration<ApplicationUserClaim>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUserClaim> builder)
+        {
+            builder.Ignore(p => p.ClaimType);
+            builder.Ignore(p => p.ClaimValue);
+
+
+            builder.HasKey(bc => new { bc.UserId, bc.OperationClaimId });
+
+            builder.HasOne(bc => bc.ApplicationUser)
+                .WithMany(b => b.ApplicationUserClaims)
+                .HasForeignKey(bc => bc.UserId);
+
+
+            builder.HasOne(bc => bc.OperationClaim)
+               .WithMany(c => c.ApplicationUserClaims)
+               .HasForeignKey(bc => bc.OperationClaimId);
+        }
+    }
+}
