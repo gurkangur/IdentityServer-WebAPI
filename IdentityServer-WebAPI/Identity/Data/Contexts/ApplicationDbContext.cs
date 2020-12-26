b8d7a0e4d0792ea17ea78a8d7b8708b655aa977a
gurkangur <gurkangur154@gmail.com>
2020-12-26 12:28:33 +0300

    Add db context

---
+++ IdentityServer-WebAPI/Identity/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer_WebAPI.Identity.Data.Entities;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace IdentityServer_WebAPI.Identity.Data.Contexts
+{
+    public class ApplicationDbContext : KeyApiAuthorizationDbContext<ApplicationUser, ApplicationRole, Guid, 
+        ApplicationUserClaim, IdentityUserRole<Guid>, IdentityUserLogin<Guid>, ApplicationRoleClaim, IdentityUserToken<Guid>>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+    }
+}
---
+++ IdentityServer-WebAPI/Identity/Data/Contexts/KeyApiAuthorizationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Extensions;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace IdentityServer_WebAPI.Identity.Data.Contexts
+{
+    public class KeyApiAuthorizationDbContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken> : 
+        IdentityDbContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken>, IPersistedGrantDbContext
+           where TUser : IdentityUser<TKey>
+           where TRole : IdentityRole<TKey>
+           where TKey : IEquatable<TKey>
+           where TUserClaim : IdentityUserClaim<TKey>
+           where TUserRole : IdentityUserRole<TKey>
+           where TUserLogin : IdentityUserLogin<TKey>
+           where TRoleClaim : IdentityRoleClaim<TKey>
+           where TUserToken : IdentityUserToken<TKey>
+    {
+        private readonly IOptions<OperationalStoreOptions> _operationalStoreOptions;
+
+
+        public KeyApiAuthorizationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options)
+        {
+            _operationalStoreOptions = operationalStoreOptions;
+        }
+
+
+        public DbSet<PersistedGrant> PersistedGrants { get; set; }
+
+
+        public DbSet<DeviceFlowCodes> DeviceFlowCodes { get; set; }
+
+        Task<int> IPersistedGrantDbContext.SaveChangesAsync() => base.SaveChangesAsync();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ConfigurePersistedGrantContext(_operationalStoreOptions.Value);
+        }
+    }
+}
